b549acd1e772bc7e576b2ef686dce6d987e03e9e
Sotiri Georgiou <tirigeorgiou@gmail.com>
2020-10-18 14:24:06 +0100

    basic funcs

---
+++ example_pkg/calculator.py
@@ -0,0 +1,44 @@
+# Series of arithmetic
+
+def add(x: int, y: int) -> int:
+    """Adds two integers.
+
+    Args:
+        x: The first integer.
+        y: The second integer to add on x.
+
+    Returns:
+        The addition of x and y.
+
+    Raises:
+        None
+
+    Examples:
+        >>> x = 4
+        >>> y = 5
+        >>> add(x, y)
+        9
+    """
+    return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    """Adds two integers.
+
+    Args:
+        x: The first integer.
+        y: The second integer to subtract from x.
+
+    Returns:
+        x subtract y.
+
+    Raises:
+        None
+
+    Examples:
+        >>> x = 12
+        >>> y = 7
+        >>> subtract(x, y)
+        5
+    """
+    return x - y

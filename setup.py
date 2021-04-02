d3b162ef21cab1cd3ff2d66def4785b648b587fe
Sotiri Georgiou <tirigeorgiou@gmail.com>
2021-04-02 10:58:15 +0100

    Only used for pip install -e

--- setup.py
+++ setup.py
@@ -1,37 +1,5 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="example-pkg-tiri1992",
-    version="0.0.1",
-    author="Tiri Georgiou",
-    author_email="tirigeorgiou@gmail.com",
-    description="A small package example",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Tiri1992/example_pkg",
-    # Add main dependencies
-    install_requires=[
-        "requests>=2.24.0"
-    ],
-    # Development dependencies: Not required to run package
-    extras_require={
-        "dev": [
-            "pytest>=3.7",
-            "autopep8==1.5.4",
-            "mypy==0.790",
-            "Sphinx==3.2.1"
-        ]
-    },
-    # Exclude the tests package
-    packages=setuptools.find_packages(exclude=['tests']),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8"
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ],
-    python_requires='>=3.8'
-)
+if __name__ == '__main__':
+    # Calls this script if you want editable pip install (-e)
+    setuptools.setup()
